e697875712023d628ddcb0552e02136d09d14944
Obed Okemsinachi <obedokemsinachi@gmail.com>
2026-02-25 12:19:56 +0100

    [fix] fixed build error

--- components/ui/apple-cards-carousel.tsx
+++ components/ui/apple-cards-carousel.tsx
@@ -17,7 +17,7 @@ import Image, { ImageProps } from "next/image";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 
 interface CarouselProps {
-  items: JSX.Element[];
+  items: React.ReactNode[];
   initialScroll?: number;
 }
 
@@ -32,7 +32,7 @@ export const CarouselContext = createContext<{
   onCardClose: (index: number) => void;
   currentIndex: number;
 }>({
-  onCardClose: () => {},
+  onCardClose: () => { },
   currentIndex: 0,
 });
 
